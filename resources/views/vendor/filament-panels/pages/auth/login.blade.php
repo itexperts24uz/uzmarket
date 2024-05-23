f52d24ec94abf8016d5545550db8f6e2cadbf5da
itexperts24uz <mrdemo@yandex.ru>
2024-05-23 05:13:55 +0500

    install

---
+++ resources/views/vendor/filament-panels/pages/auth/login.blade.php
@@ -0,0 +1,23 @@
+<x-filament-panels::page.simple>
+    @if (filament()->hasRegistration())
+        <x-slot name="subheading">
+            {{ __('filament-panels::pages/auth/login.actions.register.before') }}
+
+            {{ $this->registerAction }}
+        </x-slot>
+    @endif
+
+    {{ \Filament\Support\Facades\FilamentView::renderHook('panels::auth.login.form.before') }}
+
+    <x-filament-panels::form wire:submit="authenticate">
+        {{ $this->form }}
+
+        <x-filament-panels::form.actions
+            :actions="$this->getCachedFormActions()"
+            :full-width="$this->hasFullWidthFormActions()"
+        />
+        <center><span>&copy; {{date('Y')}} DZHALILOV K.T.</span> | <a href="https://itexperts24.uz" target="_blank">ITEXPERTS24 (v3.2.81)</a></center>
+    </x-filament-panels::form>
+
+    {{ \Filament\Support\Facades\FilamentView::renderHook('panels::auth.login.form.after') }}
+</x-filament-panels::page.simple>
